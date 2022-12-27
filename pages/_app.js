bf5221ec45ba52c2f998475fed517a0d4aea0288
MrSzasz <lugoleandro@hotmail.com>
2022-12-27 13:13:30 -0300

    gtm added

--- pages/_app.js
+++ pages/_app.js
@@ -1,3 +1,5 @@
+import { useEffect } from 'react'
+import TagManager from 'react-gtm-module'
 import { AppContext } from '../context/AppContext'
 import getFirestoreApp from '../firebase/config'
 import '../styles/globals.scss'
@@ -6,6 +8,17 @@ getFirestoreApp()
 
 function MyApp({ Component, pageProps }) {
   
+useEffect(() => {
+  
+  const tagManagerArgs = {
+    gtmId: 'GTM-KDRV8L6'
+  }
+
+  TagManager.initialize(tagManagerArgs)
+
+}, [])
+
+
   return(
     <AppContext>
       <Component {...pageProps} />
@@ -14,3 +27,21 @@ function MyApp({ Component, pageProps }) {
 }
 
 export default MyApp
+
+
+/*
+
+<!-- Google Tag Manager -->
+<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-KDRV8L6');</script>
+<!-- End Google Tag Manager -->
+Additionally, paste this code immediately after the opening <body> tag:
+<!-- Google Tag Manager (noscript) -->
+<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-KDRV8L6"
+height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+<!-- End Google Tag Manager (noscript) -->
+
+*/
