d5404c377dd66ccbaac147bfea95999f884d6c3e
MrSzasz <lugoleandro@hotmail.com>
2023-02-01 18:59:00 -0300

    env fix

--- firebase/config.js
+++ firebase/config.js
@@ -21,21 +21,13 @@ const initializeFirebaseAppIfNecessary = () => {
         return getApp();
     } catch (any) {
         const firebaseConfig = {
-            apiKey: "AIzaSyC331gkvDz4gIRP8fii485wsBrbkohqnt8",
-            authDomain: "peya-pagos.firebaseapp.com",
-            projectId: "peya-pagos",
-            storageBucket: "peya-pagos.appspot.com",
-            messagingSenderId: "146826736801",
-            appId: "1:146826736801:web:968b38ad6b94d44c53ece0"
+            apiKey:process.env.NEXT_PUBLIC_FRBS_APIKEY_ENVKEY,
+            authDomain:process.env.NEXT_PUBLIC_FRBS_AUTHDOMAIN_ENVKEY,
+            projectId:process.env.NEXT_PUBLIC_FRBS_PROJECTID_ENVKEY,
+            storageBucket:process.env.NEXT_PUBLIC_FRBS_STORAGEBUCKET_ENVKEY,
+            messagingSenderId:process.env.NEXT_PUBLIC_FRBS_MESSAGINGSENDERID_ENVKEY,
+            appId:process.env.NEXT_PUBLIC_FRBS_APPID_ENVKEY,
         };
-        // const firebaseConfig = {
-        //     apiKey: process.env.NEXT_PUBLIC_FRBS_APIKEY_ENVKEY,
-        //     authDomain: process.env.NEXT_PUBLIC_FRBS_AUTHDOMAIN_ENVKEY,
-        //     projectId: process.env.NEXT_PUBLIC_FRBS_PROJECTID_ENVKEY,
-        //     storageBucket: process.env.NEXT_PUBLIC_FRBS_STORAGEBUCKET_ENVKEY,
-        //     messagingSenderId: process.env.NEXT_PUBLIC_FRBS_MESSAGINGSENDERID_ENVKEY,
-        //     appId: process.env.NEXT_PUBLIC_FRBS_APPID_ENVKEY
-        // };
         return initializeApp(firebaseConfig);
     }
 }
