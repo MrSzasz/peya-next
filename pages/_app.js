8accc42a905ad24cff0706e61d2ec84710cd685f
MrSzasz <lugoleandro@hotmail.com>
2023-02-01 16:50:49 -0300

    hotjar added

--- pages/_app.js
+++ pages/_app.js
@@ -1,3 +1,4 @@
+import Script from 'next/script'
 import { useEffect } from 'react'
 import TagManager from 'react-gtm-module'
 import { AppContext } from '../context/AppContext'
@@ -20,9 +21,21 @@ useEffect(() => {
 
 
   return(
-    <AppContext>
-      <Component {...pageProps} />
-    </AppContext>
+    <>
+      <Script id='hotjar-analytics'>{`
+      (function(h,o,t,j,a,r){
+        h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+        h._hjSettings={hjid:3347815,hjsv:6};
+        a=o.getElementsByTagName('head')[0];
+        r=o.createElement('script');r.async=1;
+        r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+        a.appendChild(r);
+        })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
+      `}</Script>
+      <AppContext>
+        <Component {...pageProps} />
+      </AppContext>
+    </>
   )
 }
 
