f1a25e60c82123abc0551912cecdf815186a1d33
MrSzasz <lugoleandro@hotmail.com>
2023-02-04 17:28:15 -0300

    no rewrite html

--- next.config.js
+++ next.config.js
@@ -1,16 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  rewrites: async () => {
-    return [{
-      source: "/docs/tycTest",
-      destination: "/pages/docs/tyc.html",
-    },
-    {
-    source: "/docs/tyc-balonTest",
-    destination: "/pages/docs/tyc-balon.html",
-  }]
-  },i18n: {
+  i18n: {
     locales: ['es'],
     defaultLocale: 'es',
   },
