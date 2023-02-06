78133a4986cf93a6f0b9a25d514e52777323261e
MrSzasz <lugoleandro@hotmail.com>
2023-02-05 22:34:59 -0300

    revalidate on 15min

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,3 +1,98 @@
+// import Layout from "../components/Layout/Layout";
+// import styles from "./index.module.scss";
+// import Cashback from "../components/Cashback/Cashback";
+// import Benefits from "../components/Benefits/Benefits";
+// import SecureCard from "../components/SecureCard/SecureCard";
+// import Membership from "../components/Membership/Membership";
+// import PromoCarousel from "../components/PromoCarousel/PromoCarousel";
+// import Steps from "../components/Steps/Steps";
+// import Prices from "../components/Prices/Prices";
+// import Hero from "../components/Hero/Hero";
+// import {
+//   collection,
+//   getDocs,
+//   getFirestore,
+//   orderBy,
+//   query,
+// } from "firebase/firestore";
+// import { useEffect, useState } from "react";
+// import PopUp from "../components/PopUp/PopUp";
+// import { AnimatePresence } from "framer-motion";
+
+// const Home = ({ heroDataFromDB, promoDataFromDB }) => {
+//   const [showPopUp, setShowPopUp] = useState(false);
+
+//   const openModal = () => {
+//     setShowPopUp((current) => !current);
+//   };
+
+//   useEffect(() => {
+//     !localStorage.getItem("userData") &
+//       localStorage.setItem("userData", "null");
+//   }, []);
+
+//   return (
+//     <Layout fn={openModal}>
+//       <main className={styles.mainContainer}>
+//         <Hero imagesArray={heroDataFromDB} fn={openModal} />
+//         <Cashback />
+//         <Benefits fn={openModal} />
+//         <Membership fn={openModal} />
+//         <SecureCard />
+//         <PromoCarousel imagesArray={promoDataFromDB} />
+//         <Steps />
+//         <Prices />
+//       </main>
+//       <AnimatePresence>
+//         {showPopUp ? <PopUp fn={openModal} /> : null}
+//       </AnimatePresence>
+//     </Layout>
+//   );
+// };
+
+// export const getServerSideProps = async () => {
+//   try {
+//     const db = getFirestore();
+
+//     const queryCollectionHero = await query(
+//       collection(db, "hero"),
+//       orderBy("sort")
+//     );
+
+//     const heroDataFromDB = [];
+
+//     await getDocs(queryCollectionHero).then((res) =>
+//       res.docs.map((item) =>
+//         heroDataFromDB.push({ ...item.data(), id: item.id })
+//       )
+//     );
+
+//     const queryCollectionPromos = await query(
+//       collection(db, "promos"),
+//       orderBy("sort")
+//     );
+
+//     const promoDataFromDB = [];
+
+//     await getDocs(queryCollectionPromos).then((res) =>
+//       res.docs.map((item) =>
+//         promoDataFromDB.push({ ...item.data(), id: item.id })
+//       )
+//     );
+
+//     return {
+//       props: {
+//         heroDataFromDB,
+//         promoDataFromDB,
+//       },
+//     };
+//   } catch (err) {
+//     console.log(err);
+//   }
+// };
+
+// export default Home;
+
 import Layout from "../components/Layout/Layout";
 import styles from "./index.module.scss";
 import Cashback from "../components/Cashback/Cashback";
@@ -50,7 +145,7 @@ const Home = ({ heroDataFromDB, promoDataFromDB }) => {
   );
 };
 
-export const getServerSideProps = async () => {
+export const getStaticProps = async () => {
   try {
     const db = getFirestore();
 
@@ -85,6 +180,7 @@ export const getServerSideProps = async () => {
         heroDataFromDB,
         promoDataFromDB,
       },
+      revalidate: 900,
     };
   } catch (err) {
     console.log(err);
