0184536e70fa4ba4f228a91a62ea14b116edcb56
MrSzasz <lugoleandro@hotmail.com>
2023-07-11 17:48:56 -0300

    height promo hero fix

--- pages/promociones/index.jsx
+++ pages/promociones/index.jsx
@@ -79,7 +79,7 @@ const index = () => {
   };
 
   useEffect(() => {
-    getProductsFromFirebase();
+    // getProductsFromFirebase();
   }, []);
 
   useEffect(() => {
@@ -113,12 +113,19 @@ const index = () => {
         ]}
       >
         <main>
-          <div className="overflow-hidden bg-peya-blue h-[calc(80vh-var(--top-navbar))] md:h-[calc(80vh-var(--top-navbar))] mt-[var(--top-navbar)] grid grid-cols-1 md:grid-cols-promos-hero">
-            <div className="flex flex-col justify-center h-full md:px-[10%] py-[10%] md:py-0">
-              <h1 className="text-5xl text-center md:text-left md:text-7xl text-peya-turquoise font-peya-titles">70% OFF</h1>
-              <h2 className="leading-[1em] text-white text-center md:text-left text-4xl md:text-6xl">En pizzas todos los Domingos</h2>
+          <div className="overflow-hidden bg-peya-blue max-h-[calc(80vh-var(--top-navbar))] h-fit mt-[var(--top-navbar)] grid grid-cols-1 md:grid-cols-promos-hero">
+            <div className="flex flex-col self-center h-fit md:px-[10%] py-[10%] md:py-0">
+              <h1 className="text-5xl text-center md:text-left md:text-7xl text-peya-turquoise font-peya-titles">
+                70% OFF
+              </h1>
+              <h2 className="leading-[1em] text-white text-center md:text-left text-4xl md:text-6xl">
+                En pizzas todos los Domingos
+              </h2>
             </div>
-            <img src="/images/promos/promosHeroPizza.webp" className="object-cover md:object-bottom h-full w-full"/>
+            <img
+              src="/images/promos/promosHeroPizza.webp"
+              className="object-cover md:object-bottom md:h-[calc(80vh-var(--top-navbar))] h-full w-full"
+            />
           </div>
           <section className={styles.benefitsPageContainer}>
             <h2>Conoce todas nuestras promociones</h2>
